15491cd9b1bbfd676adecd7c3d684e3ceca93081
kauanatomb <kauanatomb@gmail.com>
2026-04-25 18:17:08 +0200

    done first exercise module 5

--- module05/ex00/main.c
+++ module05/ex00/main.c
@@ -1,3 +1,54 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
+#include "uart.h"
+
+static void uart_init(void)
+{
+    UBRR0H = (uint8_t)(UBRR_VALUE >> 8);
+    UBRR0L = (uint8_t)(UBRR_VALUE);
+    UCSR0B = (1 << TXEN0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); /* 8N1 */
+}
+
+static void uart_putc(char c)
+{
+    while (!(UCSR0A & (1 << UDRE0)))
+        ;
+    UDR0 = c;
+}
+
+static void uart_print_hex(uint8_t v)
+{
+    const char hex[] = "0123456789abcdef";
+    uart_putc(hex[v >> 4]);
+    uart_putc(hex[v & 0x0F]);
+    uart_putc('\r');
+    uart_putc('\n');
+}
+
+static void adc_init(void)
+{
+    /* AVCC ref, ADLAR=1 (8-bit ADCH), chanel 0 (p.257) */
+    ADMUX  = (1 << REFS0) | (1 << ADLAR);
+    /* Enable, prescaler 128 -> 125kHz 16MHz (p.258)*/
+    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+static uint8_t adc_read(void)
+{
+    // (p.258)
+    ADCSRA |= (1 << ADSC);
+    while (ADCSRA & (1 << ADSC))
+        ;
+    // (p.259)
+    return ADCH; /* 8 MSBs */
+}
+
+int main(void)
+{
+    uart_init();
+    adc_init();
+
+    while (1) {
+        uart_print_hex(adc_read());
+        _delay_ms(20);
+    }
+}
