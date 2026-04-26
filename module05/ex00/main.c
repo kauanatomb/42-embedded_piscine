9aa33128bd56eedce22a7663c6908707ad7fef9e
kauanatomb <kauanatomb@gmail.com>
2026-04-26 12:56:51 +0200

    added page to comments

--- module05/ex00/main.c
+++ module05/ex00/main.c
@@ -28,7 +28,7 @@ static void adc_init(void)
 {
     /* AVCC ref, ADLAR=1 (8-bit ADCH), chanel 0 (p.257) */
     ADMUX  = (1 << REFS0) | (1 << ADLAR);
-    /* Enable, prescaler 128 -> 125kHz 16MHz (p.258)*/
+    /* Enable, prescaler 128 -> 125kHz 16MHz (p.249, 258)*/
     ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 }
 
